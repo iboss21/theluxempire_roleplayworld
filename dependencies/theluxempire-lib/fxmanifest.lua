8bfa50b56588bd4b883555ae2f323ed2d098a135
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-07-04 21:44:21 -0500

    Create fxmanifest.lua

---
+++ dependencies/theluxempire-lib/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'iBoss21'
+description 'TheLuxEmpire Library'
+version '1.0.0'
+
+shared_scripts {
+    'shared/main.lua',
+    'shared/functions.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+lua54 'yes'
