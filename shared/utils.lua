af270c810f877042ffdab3b55ecdaba92a2ffbee
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-07-04 21:29:16 -0500

    Update utils.lua

--- shared/utils.lua
+++ shared/utils.lua
@@ -10,3 +10,59 @@ end
 
 function Utils.CalculateDistance(coords1, coords2)
     return #(
+function Utils.CalculateDistance(coords1, coords2)
+    return #(vector3(coords1.x, coords1.y, coords1.z) - vector3(coords2.x, coords2.y, coords2.z))
+end
+
+function Utils.GetRandomNumber(min, max)
+    return math.random(min, max)
+end
+
+function Utils.ShallowCopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in pairs(orig) do
+            copy[orig_key] = orig_value
+        end
+    else
+        copy = orig
+    end
+    return copy
+end
+
+function Utils.DeepCopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[Utils.DeepCopy(orig_key)] = Utils.DeepCopy(orig_value)
+        end
+        setmetatable(copy, Utils.DeepCopy(getmetatable(orig)))
+    else
+        copy = orig
+    end
+    return copy
+end
+
+function Utils.TableLength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+
+function Utils.GetTableKeys(T)
+    local keys = {}
+    for k in pairs(T) do table.insert(keys, k) end
+    return keys
+end
+
+function Utils.FormatNumber(number)
+    local i, j, minus, int, fraction = tostring(number):find('([-]?)(%d+)([.]?%d*)')
+    int = int:reverse():gsub("(%d%d%d)", "%1,")
+    return minus .. int:reverse():gsub("^,", "") .. fraction
+end
+
+return Utils
